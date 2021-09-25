36033dcc635cfc123cb009ae2de3ba4e1151c63c
Renan Maghamez Mendes <77892985+renanmmcoder@users.noreply.github.com>
2021-09-25 19:48:05 -0300

    App, alternative, css, data, index

---
+++ slider/src/data.js
@@ -0,0 +1,40 @@
+const people = [
+  {
+    id: 1,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+    name: 'Renan Mendes',
+    title: 'Developer',
+    quote:
+      'If you want to suceed, develop good habits',
+  },
+  {
+    id: 2,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    name: 'John Doe',
+    title: 'Regular Guy',
+    quote:
+      'Just another day in a regular life',
+  },
+  {
+    id: 3,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg',
+    name: 'Peter Santos',
+    title: 'Product Designer',
+    quote:
+      'Pratice sports is good 4 your health',
+  },
+  {
+    id: 4,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    name: 'Susane Andersen',
+    title: 'the boss',
+    quote:
+      'The boss ',
+  },
+];
+
+export default people;
